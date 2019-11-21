41630b908b073e7da03c7b39ecc5c59c5c303a7a
aagrawal <school@Akshays-MacBook-Pro-2.local>
2019-11-21 12:58:05 -0800

    Refactor and remove geopy

--- concatenate_data.py
+++ concatenate_data.py
@@ -20,22 +20,24 @@ other_house_pattern = re.compile(r'[^0-9]')
 apartment_pattern = re.compile(r'^[0-9]+[a-zA-Z]+$')
 
 def read_csv():
-    data1 = pd.read_csv('2014-12.csv')
-    data2 = pd.read_csv('2014-23.csv')
-    data3 = pd.read_csv('2015-1.csv')
-    data4 = pd.read_csv('2015-2.csv')
-    data5 = pd.read_csv('2015-3.csv')
-    data6 = pd.read_csv('2016-1.csv')
-    data7 = pd.read_csv('2016-2.csv')
-    data8 = pd.read_csv('2016-3.csv')
-    data9 = pd.read_csv('2017-1.csv')
-    data10 = pd.read_csv('2017-2.csv')
-    data11 = pd.read_csv('2017-3.csv')
-    data12 = pd.read_csv('2018-1.csv')
-    data13 = pd.read_csv('2018-23.csv')
-    data14 = pd.read_csv('2019-123.csv')
+    csvs = ['2014-12.csv',
+            '2014-23.csv',
+            '2015-1.csv',
+            '2015-2.csv',
+            '2015-3.csv',
+            '2016-1.csv',
+            '2016-2.csv',
+            '2016-3.csv',
+            '2017-1.csv',
+            '2017-2.csv',
+            '2017-3.csv',
+            '2018-1.csv',
+            '2018-23.csv',
+            '2019-123.csv']
 
-    return data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14
+    data = [pd.read_csv(csv) for csv in csvs]
+
+    return data
 
 def nulls_to_zeros(d):
     if pd.isnull(d):
@@ -91,20 +93,20 @@ def convert_VwSpecify(x,y):
             return 1
 
 def main():
-    data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14 = read_csv()
+    data = read_csv()
     # result = pd.concat([data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14])
-    result = data14
-    
+    result = data[13]
+
     #Rename columns
-    result = result.rename(index=str, columns={"S/A": "SubArea", 
-                                               "DOM": "DaysOnMarket", 
-                                               "Tot BR": "Bedrooms", 
-                                               "Tot Baths": "Bathrooms", 
-                                               "TotFlArea": "FloorArea", 
-                                               "Yr Blt": "YearBuilt", 
-                                               "TotalPrkng": "Parking", 
-                                               "StratMtFee": "MaintenanceFees", 
-                                               "SP Sqft": "SalePricePerSquareFoot", 
+    result = result.rename(index=str, columns={"S/A": "SubArea",
+                                               "DOM": "DaysOnMarket",
+                                               "Tot BR": "Bedrooms",
+                                               "Tot Baths": "Bathrooms",
+                                               "TotFlArea": "FloorArea",
+                                               "Yr Blt": "YearBuilt",
+                                               "TotalPrkng": "Parking",
+                                               "StratMtFee": "MaintenanceFees",
+                                               "SP Sqft": "SalePricePerSquareFoot",
                                                "TotalPrkng": "Parking"})
 
     #Remove $ and , from prices
@@ -115,16 +117,16 @@ def main():
     result['List Price'] = result['List Price'].map(lambda x: x.lstrip('$')).replace(',', '', regex=True)
     result['Sold Price'] = result['Sold Price'].map(lambda x: x.lstrip('$')).replace(',', '', regex=True)
     #print(result)
-    
+
     #Convert Parking to 0 if null
     result['Parking'] = result['Parking'].apply(nulls_to_zeros)
-    
+
     #Get HouseNumber
     result['HouseNumber'] = result['Address'].apply(get_HouseNumber)
-    
+
     #Convert to GeoPy format
     result['Address'] = result['Address'].apply(format_GeoPy)
-    
+
     #Convert VwSpecify to uppercase
     result['VwSpecify'] = result['VwSpecify'].astype(str).apply(uppercase)
     result['VwSpecify'] = result['VwSpecify'].replace('NAN', '')
@@ -144,24 +146,31 @@ def main():
     result['Locker'] = result['Locker'].replace('No', '0')
     # result['Locker'] = result['Locker'].replace('Yes', '1')
 
-    X = result[['ValueOfSubArea','Address', 'SubArea', 'DaysOnMarket', 
-                'Bedrooms', 'Bathrooms', 'FloorArea', 'YearBuilt', 'Age', 
-                'Locker', 'Parking', 'MaintenanceFees', 'SalePricePerSquareFoot', 
+    X = result[['ValueOfSubArea', 'DaysOnMarket',
+                'Bedrooms', 'Bathrooms', 'FloorArea', 'YearBuilt', 'Age',
+                'Locker', 'Parking', 'MaintenanceFees', 'SalePricePerSquareFoot',
                 'List Price', 'Sold Date', 'ValueOfView']].values
     y = result['Sold Price'].values
 
-    # #Convert address to lat and long
-    # geolocator = Nominatim(user_agent="Akshay")
-    # location = geolocator.geocode("1055 Harwood Street, Vancouver")
-    # print(location)
+    # print(result['Address'])
+    # Convert address to lat and long
+    # geolocator = Nominatim(user_agent="Akshay", timeout=60, country_bias='CA')
+    # # location = geolocator.geocode()
+    # try:
+    #     location = result.apply(lambda row: geolocator.geocode(row['Address']), axis=1)
+    #     print(location)
+    # except GeocoderTimedOut as e:
+    #     print(e)
     # print((location.latitude, location.longitude))
 
-#    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.20)
-#
-#    model = MLPRegressor(hidden_layer_sizes=(8, 6),
-#                     activation='logistic', solver='lbfgs')
-#    model.fit(X_train, y_train)
-#    print(model.score(X_valid, y_valid))
+    print(X)
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.20)
+
+    model = MLPRegressor(hidden_layer_sizes=(8, 6),
+        activation='logistic', solver='lbfgs')
+    model.fit(X_train, y_train)
+    print(model.score(X_valid, y_valid))
 
     # print(result)
     result.to_csv('2014-2019-cleaned.csv', index=False, header=True)
